653e6dd3b45afc3ad7f6d92a8f17c3067b11de4e
Emyr298 <emi.ru298@gmail.com>
2023-12-11 19:49:57 +0700

    feat: add delete and edi

--- routes/lowongan_kerja.py
+++ routes/lowongan_kerja.py
@@ -13,3 +13,11 @@ def lowongan_kerja_details(lowongan_kerja_id):
 @lowongan_kerja_blueprint.route('/new', methods=['GET'])
 def lowongan_kerja_new():
     return render_template('lowongan_kerja_add.html')
+
+@lowongan_kerja_blueprint.route('/<string:lowongan_kerja_id>/edit', methods=['GET'])
+def lowongan_kerja_edit(lowongan_kerja_id):
+    return render_template('lowongan_kerja_edit.html', lowongan_kerja_id=lowongan_kerja_id)
+
+@lowongan_kerja_blueprint.route('/<string:lowongan_kerja_id>/delete', methods=['GET'])
+def lowongan_kerja_delete(lowongan_kerja_id):
+    return render_template('lowongan_kerja_delete.html', lowongan_kerja_id=lowongan_kerja_id)
