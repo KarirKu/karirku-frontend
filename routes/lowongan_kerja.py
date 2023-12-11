760627db14e0fcb8bef0a77e1d7f4da7cc1f7831
Emyr298 <emi.ru298@gmail.com>
2023-12-11 19:02:09 +0700

    feat: add lowongan kerja

--- routes/lowongan_kerja.py
+++ routes/lowongan_kerja.py
@@ -9,3 +9,7 @@ def lowongan_kerja_index():
 @lowongan_kerja_blueprint.route('/<string:lowongan_kerja_id>', methods=['GET'])
 def lowongan_kerja_details(lowongan_kerja_id):
     return render_template('lowongan_kerja_details.html', lowongan_kerja_id=lowongan_kerja_id)
+
+@lowongan_kerja_blueprint.route('/new', methods=['GET'])
+def lowongan_kerja_new():
+    return render_template('lowongan_kerja_add.html')
